c4212aaa0bf99a380efa66e1b6413a2248f1b13e
sidneyboy <100180350+sidneyboy@users.noreply.github.com>
2024-07-31 09:58:12 +0800

    dasasdasd

--- resources/views/van_selling_ledger.blade.php
+++ resources/views/van_selling_ledger.blade.php
@@ -26,10 +26,13 @@
              </div>
              <div class="card-body">
                  <div class="table table-responsive">
-                     <table class="table table-bordered table-sm table-striped table-hover" style="font-size:11px" id="example2">
+                     <table class="table table-bordered table-sm table-striped table-hover export_table" style="font-size:11px"
+                         id="example2">
                          <thead>
                              <tr>
                                  <th>Principal</th>
+                                 <th>Code</th>
+                                 <th>Type</th>
                                  <th>Description</th>
                                  <th>Qty</th>
                              </tr>
@@ -38,19 +41,22 @@
                              @for ($i = 0; $i < count($sku_ledger); $i++)
                                  <tr>
                                      <td>{{ $sku_ledger[$i]->principal }}</td>
-                                     <td><b style="color:green">{{ $sku_ledger[$i]->sku_code }}</b> -
-                                         {{ $sku_ledger[$i]->description }}<br />
-                                         <b style="color:blue">{{ $sku_ledger[$i]->sku_type }}</b>
-
-                                        </td>
+                                     <td>{{ $sku_ledger[$i]->sku_code }}</td>
+                                     <td>{{ $sku_ledger[$i]->sku_type }}</td>
+                                     <td>{{ $sku_ledger[$i]->description }}</td>
                                      <td style="text-align: right">{{ $sku_ledger[$i]->running_balance }}</td>
                                  </tr>
                              @endfor
                          </tbody>
                      </table>
                  </div>
+
+                 <br />
+
+                 <button id="btnExport" type="button" class="btn btn-info btn-block btn-sm"><i class="fa fa-file-csv"></i> Export to CSV</button>
              </div>
          </div>
+         <input type="hidden" id="agent_name" value="{{ $agent_user->full_name }} INVENTORY.csv">
      </section>
  @endsection
 
@@ -64,39 +70,100 @@
              }
          });
 
-         $("#van_selling_ledger_generate").on('submit', (function(e) {
-             e.preventDefault();
-             $('.loading').show();
-             $.ajax({
-                 url: "van_selling_ledger_generate",
-                 type: "POST",
-                 data: new FormData(this),
-                 contentType: false,
-                 cache: false,
-                 processData: false,
-                 success: function(data) {
-                     if (data == 'NO_DATA_FOUND') {
-                         Swal.fire(
-                             'NO DATA FOUND!',
-                             'CANNOT PROCEED!',
-                             'error'
-                         )
-                         $('.loading').hide();
-                     } else {
-                         $('#van_selling_ledger_generate_page').html(data);
-                         $('.loading').hide();
-                     }
-                 },
-                 error: function(error) {
-                     $('.loading').hide();
-                     Swal.fire(
-                         'Cannot Proceed',
-                         'Please Contact IT Support',
-                         'error'
-                     )
+         //  $("#van_selling_ledger_generate").on('submit', (function(e) {
+         //      e.preventDefault();
+         //      $('.loading').show();
+         //      $.ajax({
+         //          url: "van_selling_ledger_generate",
+         //          type: "POST",
+         //          data: new FormData(this),
+         //          contentType: false,
+         //          cache: false,
+         //          processData: false,
+         //          success: function(data) {
+         //              if (data == 'NO_DATA_FOUND') {
+         //                  Swal.fire(
+         //                      'NO DATA FOUND!',
+         //                      'CANNOT PROCEED!',
+         //                      'error'
+         //                  )
+         //                  $('.loading').hide();
+         //              } else {
+         //                  $('#van_selling_ledger_generate_page').html(data);
+         //                  $('.loading').hide();
+         //              }
+         //          },
+         //          error: function(error) {
+         //              $('.loading').hide();
+         //              Swal.fire(
+         //                  'Cannot Proceed',
+         //                  'Please Contact IT Support',
+         //                  'error'
+         //              )
+         //          }
+         //      });
+         //  }));
+
+         class csvExport {
+             constructor(table, header = true) {
+                 this.table = table;
+                 this.rows = Array.from(table.querySelectorAll("tr"));
+                 if (!header && this.rows[0].querySelectorAll("th").length) {
+                     this.rows.shift();
                  }
+                 // console.log(this.rows);
+                 // console.log(this._longestRow());
+             }
+
+             exportCsv() {
+                 const lines = [];
+                 const ncols = this._longestRow();
+                 for (const row of this.rows) {
+                     let line = "";
+                     for (let i = 0; i < ncols; i++) {
+                         if (row.children[i] !== undefined) {
+                             line += csvExport.safeData(row.children[i]);
+                         }
+                         line += i !== ncols - 1 ? "," : "";
+                     }
+                     lines.push(line);
+                 }
+                 //console.log(lines);
+                 return lines.join("\n");
+             }
+             _longestRow() {
+                 return this.rows.reduce((length, row) => (row.childElementCount > length ? row.childElementCount :
+                     length), 0);
+             }
+             static safeData(td) {
+                 let data = td.textContent;
+                 //Replace all double quote to two double quotes
+                 data = data.replace(/"/g, `""`);
+                 //Replace , and \n to double quotes
+                 data = /[",\n"]/.test(data) ? `"${data}"` : data;
+                 return data;
+             }
+         }
+
+         const btnExport = document.querySelector("#btnExport");
+         const tableElement = document.querySelector(".export_table");
+
+         btnExport.addEventListener("click", () => {
+             const obj = new csvExport(tableElement);
+             const csvData = obj.exportCsv();
+             const blob = new Blob([csvData], {
+                 type: "text/csv"
              });
-         }));
+             const url = URL.createObjectURL(blob);
+             const a = document.createElement("a");
+             a.href = url;
+             a.download = $('#agent_name').val();
+             a.click();
+
+             setTimeout(() => {
+                 URL.revokeObjectURL(url);
+             }, 500);
+         });
      </script>
      </body>
 
